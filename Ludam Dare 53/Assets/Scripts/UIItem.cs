17e0fbac8bbccaaa825e13d20018271747753f49
Imated <lineborder122@gmail.com>
2023-05-01 19:52:44 -0400

    Updated Formula

--- Ludam Dare 53/Assets/Scripts/UIItem.cs
+++ Ludam Dare 53/Assets/Scripts/UIItem.cs
@@ -42,7 +42,7 @@ public class UIItem : MonoBehaviour
         referenceItem = item;
         _sellingPrice = price;
         _isTransaction = true;
-        _chanceOfBeingSold = Mathf.Pow(50, Mathf.Pow(item.cost / price, 1 / (item.cost / price))) / 100;
+        _chanceOfBeingSold = Mathf.Clamp01(Mathf.Pow((item.cost / price), 0.6f) * 0.7f);
         print(price);
         print(_chanceOfBeingSold * 100 + "%");
     }
